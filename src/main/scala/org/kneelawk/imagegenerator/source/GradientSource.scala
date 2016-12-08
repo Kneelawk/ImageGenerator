fd0fdb9b1adbd11810a8cfe506cc89dbf1842aeb
Kneelawk <jedidiah.pommert@gmail.com>
2016-12-08 01:30:37 -0800

    Begin on gradient source

---
+++ src/main/scala/org/kneelawk/imagegenerator/source/GradientSource.scala
@@ -0,0 +1,7 @@
+package org.kneelawk.imagegenerator.source
+
+import org.kneelawk.imagegenerator.ImageSource
+
+class GradientSource extends ImageSource {
+  def apply(x: Int, y: Int) = null // TODO: gradient stuff
+}
