8319ab53db4cc2ab456bdcc5aad56f532215036b
Kneelawk <jedidiah.pommert@gmail.com>
2016-12-09 13:08:57 -0800

    Add more to vec2f

--- src/main/scala/org/kneelawk/imagegenerator/Vec2.scala
+++ src/main/scala/org/kneelawk/imagegenerator/Vec2.scala
@@ -1,7 +1,16 @@
 package org.kneelawk.imagegenerator
 
 case class Vec2f(x: Float, y: Float) {
-  def dot(m: Float) = new Vec2f(x * m, y * m)
+  val r: Float = Math.sqrt(x * x + y * y).toFloat
   
+  /**
+   * Theta will often be clockwise in images.
+   */
+  val theta: Float = Math.atan2(y, x).toFloat
+
+  def dot(m: Float) = new Vec2f(x * m, y * m)
+
   def dot(v: Vec2f) = x * v.x + y * v.y
+
+  def normalize = new Vec2f(x / r, y / r)
 }
